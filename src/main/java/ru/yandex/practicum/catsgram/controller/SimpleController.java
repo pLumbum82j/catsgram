6443e54130aa79e536689a674cd77649ddebbd8c
pLumbum82 <jobbsy@yandex.ru>
2023-01-11 17:26:45 +0500

    add catsgram

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+//    @RequestMapping("/home")
+//    public String homePage() {
+//        return "Котограм";
+//    }
+    @GetMapping("/home")
+    public String homePage() {
+        return "Котограм";
+    }
+}
